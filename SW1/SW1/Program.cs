392ebddcbe2b0852fcc9accfc4a1411cfdd8076a
Malenci <138701443+Malenci@users.noreply.github.com>
2025-04-24 23:56:53 +0300

    commit
    s

---
+++ SW1/SW1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SW1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Автор: Холявко Богдан Станіславович; варіант №23; група: БІКСб-2-24-4.0д\n");
+            int number;
+
+            while (true)
+            {
+                Console.Write("Введіть чотиризначне натуральне число: ");
+                if (int.TryParse(Console.ReadLine(), out number) && number >= 1000 && number <= 9999)
+                    break;
+
+                Console.WriteLine("Помилка! Потрібно ввести чотиризначне натуральне число.");
+            }
+
+            int digit1 = number / 1000;
+            int digit2 = (number / 100) % 10;
+            int digit3 = (number / 10) % 10;
+            int digit4 = number % 10;
+
+            digit3 = (digit3 == 9) ? 0 : digit3 + 1;
+
+            int newNumber = digit1 * 1000 + digit2 * 100 + digit3 * 10 + digit4;
+
+            Console.WriteLine($"Перетворене число: {newNumber}");
+        }
+    }
+}
---
+++ SW1/SW1/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("SW1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SW1")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("e173de3f-ffc1-4467-8b1e-019da0e035f0")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
